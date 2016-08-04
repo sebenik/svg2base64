9217f095d31ac294399f134bf2cf816b7b464573
Žiga Šebenik <ziga@sebenik.com>
2016-08-04 06:55:01 -0700

    modify to include updates of v1.6

--- example/example.js
+++ example/example.js
@@ -1,10 +1,40 @@
+"use strict";
+
 var svg2base64 = require('../lib/svg2base64.js');
 
-var svg = '<svg xmlns="http://www.w3.org/2000/svg" width="300px" height="80"><text x="150" y="40" dy=".4em" font-size="50px" text-anchor="middle">Hello World!</text></svg>';
+var svg =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="300px" height="80">'
+  + '<text x="150" y="40" dy=".4em" font-size="50px" text-anchor="middle">'
+  + 'Hello World!'
+  + '</text>'
+  + '</svg>';
 
-svg2base64(svg,
-	{
-		"scale":4,
-		"id":"mySvg",
-		"type":'png'
-	});
+var scale = 4;
+
+svg2base64(svg, {
+  "scale": scale,
+  "id": "svg",
+  "type": 'png'
+});
+
+svg2base64(svg, {
+  "scale": scale,
+  "id": "svgCss",
+  "type": 'png',
+  "css": "text { fill: #735ce8; stroke: #33d8b8; }"
+});
+
+svg2base64(svg, {
+  "scale": scale,
+  "id": "svgWithGradient",
+  "type": 'png',
+  "defs": 
+  "<linearGradient id='gradient' "
+  + "x1='0' y1='0' x2='" + 300 * scale + "' y2='0' "
+  + "gradientUnits='userSpaceOnUse'> "
+  + "<stop offset='0%' style='stop-color:#735ce8'/> "
+  + "<stop offset='100%' style='stop-color:#33d8b8'/> "
+  + "</linearGradient>",
+  "css": "text { fill: url(#gradient); }"
+});
+
--- example/run.php
+++ example/run.php
@@ -1,15 +1,12 @@
 #!/usr/bin/php
 <?php
-	exec('node example.js', $out, $ret);
+exec('node example.js', $out, $ret);
 
-	foreach($out as $b64)
-	{
-		$data = explode(' ',trim($b64));
-		if(sizeof($data) == 2)
-		{
-			$id    = $data[0];
-			$image = $data[1];
-			file_put_contents($id.'.png', base64_decode($image));
-		}
-	}
-?>
+foreach($out as $b64) {
+  $data = explode(' ',trim($b64));
+  if(sizeof($data) == 2) {
+    $id = $data[0];
+    $image = $data[1];
+    file_put_contents($id.'.png', base64_decode($image));
+  }
+}
